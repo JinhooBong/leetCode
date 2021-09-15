3847c41c48002c864ce7aa21dd9ec3ddc7a61092
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-14 18:00:19 -0700

    setting up longestCommonPrefix

---
+++ removeDuplicates/removeDuplicates.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+/**
+ * Write a function to find the longest common prefix amongst an array of strings
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ * input: strs = ['flower', 'flow', 'flight']
+ * output: 'fl'
+ *
+ * Example 2:
+ * input: strs = ['dog', 'racecar', 'car']
+ * output: ""
+ */
+
+ var longestCommonPrefix = function(strs) {
+
+};
