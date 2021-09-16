0efa43bb60c99d59dc12b9082cf72d33851db810
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-16 10:07:05 -0700

    longestSubstring w/o repeating

---
+++ longestSubstring/longestSubstring.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * 
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * 
+ * Example 1:
+ * input: s = "abcabcbb"
+ * output: 3 ("abc")
+ *
+ * 
+ * Example 2: 
+ * input: s = "b"
+ * output: 1
+ * 
+ * Example 3:
+ * input: s = "pwwkew"
+ * output: 3
+ * 
+ * Example 4: 
+ * input: s = ""
+ * output: 0 
+ */
+ var lengthOfLongestSubstring = function(s) {
+    
+};
