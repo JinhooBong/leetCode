c1a983a338b0f708c03b650fdfe6cf47ab87b0cc
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-20 16:19:04 -0700

    attempt at spiral matrix

--- spiralMatrix/spiralMatrix.js
+++ spiralMatrix/spiralMatrix.js
@@ -13,7 +13,73 @@
  * output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
  */
  var spiralOrder = function(matrix) {
+    if(matrix.length === 0 || matrix[0].length === 0) {
+        return matrix;
+    }
 
+    // if the matrix length is 2, then we simply return the first array printed correctly
+    // and then we return the array at length backwards
+
+    // variable to keep track of spun matrix
+    let spunMatrix = [];
+    // for(let i = 0; i < matrix.length; i++) {
+    //     if(i === 0) {
+    //         for (let j = 0; j < matrix[i].length; j++){
+    //             spunMatrix.push(matrix[i][j]);
+    //         }
+    //     } else {
+    //         for (let j = matrix[i].length - 1; j >= 0; j--) {
+    //             spunMatrix.push(matrix[i][j]);
+    //         }
+    //     }
+    // }
+
+    // we want to traverse right, 
+    // then traverse down,
+    // then traverse left, 
+    // then traverse up
+
+    let startRowPos = 0;
+    let endRowPos = matrix.length - 1;
+    let startColPos = 0;
+    let endColPos = matrix[0].length - 1;
+
+    while (startRowPos <= endRowPos && startColPos <= endColPos) {
+
+        // traverse right
+        for (let i = startColPos; i <= endColPos; i++) {
+            spunMatrix.push(matrix[startRowPos][i]);
+        }
+        // increment row
+        startRowPos++;
+
+        // traverse down (keep column position, but increment row)
+        for (let i = startRowPos; i <= endRowPos; i++) {
+            spunMatrix.push(matrix[i][endColPos]);
+        }
+        // decrement col position 
+        endColPos--;
+
+        // traverse left (keep row position, and decrement col)
+        // when we traverse left, we want to check if the row or column exists in order to prevent duplicates
+        if (startRowPos <= endRowPos) {
+            for (let i = endColPos; i >= startColPos; i--) {
+                spunMatrix.push(matrix[endRowPos][i]);
+            }
+        }   
+        endRowPos--;
+
+        // if column exists, then traverse up
+        if(startColPos <= endColPos) {
+            for (let i = endRowPos; i >= startRowPos; i--) {
+                spunMatrix.push(matrix[i][startColPos]);
+            }
+        }
+        startColPos++;
+
+    }
+
+    return spunMatrix;
 };
 
 module.exports = spiralOrder;
--- spiralMatrix/spiralMatrix.test.js
+++ spiralMatrix/spiralMatrix.test.js
@@ -6,4 +6,18 @@ describe('Spiral Matrix', () => {
         expect(typeof spiralMatrix).toBe('function');
     });
 
+    it('Should return correct base cases', () => {
+        expect(spiralMatrix([[]])).toStrictEqual([[]]);
+        expect(spiralMatrix([[1]])).toStrictEqual([1]);
+
+    })
+
+    it('Should return [1, 2, 3, 6, 5, 4] for [[1, 2, 3], [4, 5, 6]', () => {
+        expect(spiralMatrix([[1, 2, 3], [4, 5, 6]])).toStrictEqual([1, 2, 3, 6, 5, 4]);
+    });
+
+    it('Should return [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]', () => {
+        expect(spiralMatrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])).toStrictEqual([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]);
+    })
+
 });
