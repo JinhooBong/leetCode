2257765f747f47d97970d619eb4821c496a23015
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-20 14:51:48 -0700

    setting up test for spiral matrix

---
+++ spiralMatrix/spiralMatrix.test.js
@@ -0,0 +1,9 @@
+const spiralMatrix = require('./spiralMatrix.js');
+
+describe('Spiral Matrix', () => {
+
+    it('Should be a function', () => {
+        expect(typeof spiralMatrix).toBe('function');
+    });
+
+});
