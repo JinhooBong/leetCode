bfa0d95fd769a7c3ef6dd8c15baf30a0fed0cc03
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-20 14:42:55 -0700

    setting up arrays problems from tech blueprint

---
+++ equivalentFoldedSum/equivalentFoldedSum.js
@@ -0,0 +1,16 @@
+/**
+ * You are given a list of integers nums of even length. 
+ * Consider an operation where you pick any number in nums 
+ * and update it with a value between [1, max(nums)].
+
+ * Return the minimum number of operations required 
+ * such that for every i, nums[i] + nums[n - 1 - i] equals 
+ * to the same number.
+ */
+
+
+var equivalentFoldedSums = (nums) => {
+
+}
+
+module.exports = equivalentFoldedSums;
---
+++ equivalentFoldedSum/equivalentFoldedSum.test.js
@@ -0,0 +1,9 @@
+const foldedSum = require('./equivalentFoldedSum.js');
+
+describe('Equivalent Folded Sum', () => {
+
+    it('Should be a function', () => {
+        expect(typeof foldedSum).toBe('function');
+    })
+
+});
