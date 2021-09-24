357de2f404fa704f627d04fb56a105c1ad6e0ca7
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-24 11:04:18 -0700

    setting up new challenge problems for 9/24"

---
+++ nthTribonnaci/nthTribonacci.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ *
+ * The Tribonacci sequence T_n is defined as follows :
+ *
+ * T_0 = 0, T_1 = 1, T_2 = 1, T_n+3 = T_n + T_n+1 + T_n+2 for n >= 0
+ *
+ * Given n, return the value of T_n
+ *
+ *
+ * EAMPLE:
+ * input: n = 4
+ * output: 4
+ * explanation: T_3 = 0 + 1 + 1 = 2
+ * T_4 = 1 + 1 + 2 = 4
+ *
+ * EXAMPLE:
+ * input: n = 25
+ * output: 1389537
+*/
+var tribonacci = function(n) {
+
+};
+
+module.exports = tribonacci;
