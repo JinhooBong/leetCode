83b5995b09bcc1c50d7c59b94e1fb59386916211
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-23 23:03:51 -0700

    set up testing for expression operator

---
+++ expressionOperators/expressionOperator.test.js
@@ -0,0 +1,9 @@
+const operator = require('./expressionOperators.js');
+
+describe('Expression Operators', () => {
+
+    it('Should be a function', () => {
+        expect(typeof operator).toBe('function');
+    })
+
+});
