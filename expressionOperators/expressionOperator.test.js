76deb5596eccc299cf1b9ee358b3924df1de979f
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-23 23:49:24 -0700

    attempt at expression operator - not handling all cases. still need to figure out rest of problem

--- expressionOperators/expressionOperator.test.js
+++ expressionOperators/expressionOperator.test.js
@@ -4,6 +4,17 @@ describe('Expression Operators', () => {
 
     it('Should be a function', () => {
         expect(typeof operator).toBe('function');
+    });
+
+    it('Should return 0s', () => {
+        expect(operator('00', 0)).toStrictEqual(['0*0', '0+0', '0-0']);
+    });
+
+    it('Should return 123s', () => {
+        expect(operator('123', 6)).toStrictEqual(['1*2*3', '1+2+3']);
+    });
+    it('Should return 232', () => {
+        expect(operator('232', 8)).toStrictEqual(['2*3+2', '2+3*2']);
     })
 
 });
--- expressionOperators/expressionOperators.js
+++ expressionOperators/expressionOperators.js
@@ -21,7 +21,70 @@
  * intput: num="3456237490", target=9191
  * output:[]
  */
- var addOperators = function(num, target) {
+var addOperators = function(num, target) {
+
+    // input: a string of digits, and an integer target
+    // output: an array
+
+    let operatorArr = [];
+
+    // split the number 
+    let digits = num.split('');
+    // as we go through each digit,
+    // see what the product is, summation, subtraction
+    // check whether any of them equal target
+    // if so, then return the digits with the corresponding expression 
+
+    let product = 1;
+    let sum = 0;
+    let subtract = 0;
+
+    for (let i = 0; i < digits.length; i++) {
+        product *= parseInt(digits[i]);
+        sum += parseInt(digits[i]);
+        subtract -= parseInt(digits[i]);
+    }
+
+    if (product === target) {
+        let productStr = '';
+        for (let i = 0; i < digits.length; i++) {
+            productStr += digits[i];
+            if (i === digits.length - 1) {
+                // nothing
+            } else {
+                productStr += '*';
+            }
+        }
+        operatorArr.push(productStr);
+    }
+
+    if (sum === target) {
+        let sumStr = '';
+        for (let i = 0; i < digits.length; i++) {
+            sumStr += digits[i];
+            if (i === digits.length - 1) {
+                // nothing
+            } else {
+                sumStr += '+';
+            }
+        }
+        operatorArr.push(sumStr);
+    }
+
+    if (subtract === target) {
+        let minusStr = '';
+        for (let i = 0; i < digits.length; i++) {
+            minusStr += digits[i];
+            if (i === digits.length - 1) {
+                // nothing
+            } else {
+                minusStr += '-';
+            }
+        }
+        operatorArr.push(minusStr);
+    }
+
+    return operatorArr;
 
 };
 
