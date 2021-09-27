d0bbe42801b331d5da51cdd991eb9c369dd4fcad
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-27 15:40:09 -0700

    working attempt with local tests

--- nthTribonnaci/nthTribonacci.test.js
+++ nthTribonnaci/nthTribonacci.test.js
@@ -6,4 +6,21 @@ describe('Nth Tribonnaci', () => {
         expect(typeof nthTrib).toBe('function');
     });
 
+    it('Should handle base cases', () => {
+        expect(nthTrib(0)).toBe(0);
+        expect(nthTrib(1)).toBe(1);
+        expect(nthTrib(2)).toBe(1);
+    });
+
+    it('Should be 2', () => {
+        expect(nthTrib(3)).toBe(2);
+    })
+
+    it('Should be 4', () => {
+        expect(nthTrib(4)).toBe(4);
+    });
+
+    it('Should be 1389537', () => {
+        expect(nthTrib(25)).toBe(1389537);
+    })
 });
