24067006bc2a658d4fe24901d8e94b9c4be56d13
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-27 15:19:48 -0700

    setting up test for nth trib

---
+++ nthTribonnaci/nthTribonacci.test.js
@@ -0,0 +1,9 @@
+const nthTrib = require('./nthTribonacci.js');
+
+describe('Nth Tribonnaci', () => {
+   
+    it('Should be a function', () => {
+        expect(typeof nthTrib).toBe('function');
+    });
+
+});
