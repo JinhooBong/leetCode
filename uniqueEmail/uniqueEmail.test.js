b9a1fd5efef737cd1942f3e8d70bfa974a604b7e
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-27 14:42:23 -0700

    setting up tests

--- uniqueEmail/uniqueEmail.test.js
+++ uniqueEmail/uniqueEmail.test.js
@@ -6,4 +6,15 @@ describe('Number of Unique Email Addresses', () => {
         expect(typeof uniqueEmails).toBe('function');
     });
 
+    it('Should return 0 for empty', () => {
+        expect(uniqueEmails([])).toBe(0);
+    });
+
+    it('Should return 2', () => {
+        expect(uniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"])).toBe(2);
+    });
+
+    it('Should return 3', () => {
+        expect(uniqueEmails(["a@leetcode.com","b@leetcode.com","c@leetcode.com"])).toBe(3);
+    });
 });
