218bd6c3288339d8b0347661956d877c96d04390
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-27 18:06:34 -0700

    passing local tests

--- 1d_runningSum/runningSum1D.test.js
+++ 1d_runningSum/runningSum1D.test.js
@@ -4,6 +4,18 @@ describe('Running Sum of 1 Dimensional Array', () => {
 
     it('Should be a function', () => {
         expect(typeof runSum).toBe('function');
+    });
+
+    it('Should return correctly', () => {
+        expect(runSum([1, 2, 3, 4])).toStrictEqual([1, 3, 6, 10]);
+    })
+
+    it('Should return correctly', () => {
+        expect(runSum([1, 1, 1, 1, 1])).toStrictEqual([1, 2, 3, 4, 5]);
+    });
+
+    it('Should return correctly', () => {
+        expect(runSum([3, 1, 2, 10, 1])).toStrictEqual([3, 4, 6, 16, 17]);
     })
 
 });
