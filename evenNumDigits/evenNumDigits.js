c3727e4d7b0acb0af4c573b71760d445976dca67
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-28 21:36:02 -0700

    evenNumdigits set up

---
+++ evenNumDigits/evenNumDigits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * Given an array nums of integers, return how many of them contain an even number of digits.
+ */
+var findNumbers = function(nums) {
+    
+};
+
+module.exports = findNumbers;
---
+++ evenNumDigits/evenNumDigits.test.js
@@ -0,0 +1,9 @@
+const findNumbers = require('./evenNumDigits');
+
+describe('Find numbers with even digits', () => {
+
+    it('Should be a function', () => {
+        expect(typeof findNumbers).toBe('function');
+    })
+
+});
