c669a7492d5193eeb87b43ed5aacb6601e796f10
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-29 00:41:08 -0700

    split list set up with test

---
+++ splitList/splitListToParts.js
@@ -0,0 +1,23 @@
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ *
+ *
+ * Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+ * The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+ * The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+ * Return an array of the k parts.
+ */
+
+// Definition for singly-linked list.
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
+
+var splitListToParts = function(head, k) {
+
+};
+
+module.exports = splitListToParts;
---
+++ splitList/splitListToParts.test.js
@@ -0,0 +1,9 @@
+const splitList = require('./splitListToParts.js');
+
+describe('Split List to Parts', () => {
+
+  it('Should be a function', () => {
+    expect(typeof splitList).toBe('function');
+  })
+
+})
