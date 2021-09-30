db675cbee8c85ad3ccb4768f2e7dc1dce0ef9121
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-30 15:58:35 -0700

    attempt passing local tests

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -8,7 +8,30 @@
  * 
  */
 var majorityElement = function(nums) {
-    
+    // input: an array of nums
+    // output: the majority element
+
+    // create an object to keep track of how many times the number appears
+    // grab the size of n/2
+    // if the number of times an element appears is greater than half then return that element
+
+    let counterObj = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (counterObj[nums[i]] === undefined) {
+            counterObj[nums[i]] = 1;
+        } else {
+            counterObj[nums[i]]++;
+        }
+    }
+
+    let half = Math.floor(nums.length / 2);
+
+    for (let key in counterObj) {
+        if (counterObj[key] > half) {
+            return parseInt(key);
+        }
+    }
 };
 
 module.exports = majorityElement;
--- majorityElement/majorityElement.test.js
+++ majorityElement/majorityElement.test.js
@@ -4,6 +4,14 @@ describe('Majority Element', () => {
 
     it('Should be a function', () => {
         expect(typeof majorityElement).toBe('function');
-    })
+    });
+
+    it('Should return 3', () => {
+        expect(majorityElement([3, 2, 3])).toBe(3);
+    });
+
+    it('Should return 2', () => {
+        expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).toBe(2);
+    });
 
 });
