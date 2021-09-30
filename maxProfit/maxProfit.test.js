30a101f2565a21438e30e0eafbe3dc65c9a83d34
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-30 16:02:13 -0700

    max profit problem set up

---
+++ maxProfit/maxProfit.test.js
@@ -0,0 +1,9 @@
+const maxProfit = require('./maxProfit');
+
+describe('When to buy and sell stocks', () => {
+
+    it('Should be a function', () => {
+        expect(typeof maxProfit).toBe('function');
+    })
+
+});
