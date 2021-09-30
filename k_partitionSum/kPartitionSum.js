f032c10824e6c8275a78e4671f645a124184d7ea
Jinhoo Bong <jinhoobong@gmail.com>
2021-09-30 13:52:02 -0700

    leetcode challenge 9/30

---
+++ k_partitionSum/kPartitionSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ * 
+ * Given an integer array nums and an integer k, return true if it is possible to divide this array into k non-empty subsets whose sums are all equal.
+ * 
+ */
+var canPartitionKSubsets = function(nums, k) {
+    
+};
+
+module.exports = canPartitionKSubsets;
+
---
+++ k_partitionSum/kPartitionSum.test.js
@@ -0,0 +1,9 @@
+const kSum = require('./kPartitionSum');
+
+describe('Can Partition K Subsets', () => {
+
+    it('Should be a function', () => {
+        expect(typeof kSum).toBe('function');
+    })
+
+});
