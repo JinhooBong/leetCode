b7633437c0fd922c2be5648d50046ad5a7db339b
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-04 15:36:45 -0700

    island perimeter problem setup

---
+++ islandPerimeter/islandPerimeter.test.js
@@ -0,0 +1,9 @@
+const islandPerimeter = require('./islandPerimeter');
+
+describe('Island Perimeter problem', () => {
+
+    it('Should be a function', () => {
+        expect(typeof islandPerimeter).toBe('function');
+    });
+
+});
