7582dd402775fa9beae59555410e596d18d942d7
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-05 14:55:27 -0700

    climbing stairs problem set up

--- climbingStairs/climbingStairs.test.js
+++ climbingStairs/climbingStairs.test.js
@@ -1,2 +1,9 @@
 const climbStairs = require('./climbingStairs');
 
+describe('Climbing Stairs', () => {
+
+  it('Should be a function', () => {
+    expect(typeof climbStairs).toBe('function');
+  });
+
+});
