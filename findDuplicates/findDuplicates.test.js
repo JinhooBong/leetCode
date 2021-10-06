c0836d464ce1e09b3d454f59396dde2cfec982e5
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-06 13:57:52 -0700

    find duplicates problem set up

---
+++ findDuplicates/findDuplicates.test.js
@@ -0,0 +1,9 @@
+const findDuplicates = require('./findDuplicates');
+
+describe('Find Duplicates', () => {
+
+    it('Should be a function', () => {
+        expect(typeof findDuplicates).toBe('function');
+    })
+
+});
