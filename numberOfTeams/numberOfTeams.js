9265e0a708597a6aa6f0f91b031ee7be1bee364f
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-06 15:21:08 -0700

    found different leetcode solution

--- numberOfTeams/numberOfTeams.js
+++ numberOfTeams/numberOfTeams.js
@@ -54,6 +54,16 @@ var numTeams = function(rating) {
         }
     }
 
+    // for (let i = 0; i < rating.length; i++) {
+    //     for (let j = i + 1; j < rating.length; j++) {
+    //         for (let k = j + 1; k < rating.length; k++) {
+    //             if (rating[i] > rating[j] && rating[j] > rating[k] || rating[i] < rating[j] && rating[j] < rating[k]) {
+    //                 numOfTeams++;
+    //             }
+    //         }
+    //     }
+    // }
+
     // return count
     return numOfTeams;
 };
