67750a0c56f37f5d3ad54d3c5d069c79b75b8ac9
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-12 16:13:29 -0700

    rotate passing local test

--- rotate/rotate.js
+++ rotate/rotate.js
@@ -1,9 +1,44 @@
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
+ * 
+ * You are given an n x n 2D matrix representing an image, rotate the
+ * image by 90 degrees (clockwise).
+ * 
+ * You have to rotate the image in-place, which means you have to modify
+ * the input 2D matrix directly. DO NOT allocate another 2D matrix and
+ * do the rotation.
+ * 
+ * 
+ * example: matrix = 
+ * [[1,2,3]
+ * [4,5,6]
+ * [7,8,9]]
+ * output: 
+ * [[7,4,1]
+ *  [8,5,2]
+ *  [9,6,3]]
+ * 
  */
 var rotate = function(matrix) {
-    
+    // input: 2d matrix 
+    // output: a 2d matrix rotated 90deg right
+
+    // [0][0] becomes [0][n]
+    // [0][1] becomes [1][n]
+    // [0][2] becomes [2][n]
+
+    // [1][0] becomes [0][1]
+    // [1][1] becomes [1][1]
+    // [1][2] becomes [2][1]
+
+    // [2][0] becomes [0][0]
+    // [2][1] becomes [1][0]
+    // [2][2] becomes [2][0]
+
+    return matrix.map((row, i) =>
+        row.map((val, j) => matrix[matrix.length - 1 - j][i])
+    );
 };
 
 module.exports = rotate;
--- rotate/rotate.test.js
+++ rotate/rotate.test.js
@@ -4,6 +4,33 @@ describe('Rotate', () => {
 
     it('Should be a function', () => {
         expect(typeof rotate).toBe('function');
+    });
+
+    it('Should rotate', () => {
+        let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+        expect(rotate(matrix)).toStrictEqual([
+            [7, 4, 1], 
+            [8, 5, 2],
+            [9, 6, 3]
+        ]);
+    });
+
+    it('Should rotate 2', () => {
+        let matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
+
+        expect(rotate(matrix)).toStrictEqual([[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]);
+    })
+
+    it('Should rotate base case', () => {
+        let matrix = [[1]];
+        expect(rotate(matrix)).toStrictEqual([[1]]);
+    });
+
+    it('Should rotate 3', () => {
+        let matrix = [[1,2],[3,4]];
+
+        expect(rotate(matrix)).toStrictEqual([[3,1],[4,2]]);
     })
 
 });
