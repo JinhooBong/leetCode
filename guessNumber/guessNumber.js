a7c4802aa3ba01794d6e6f75b560761a465a29b3
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-11 23:04:41 -0700

    attempt guess number

--- guessNumber/guessNumber.js
+++ guessNumber/guessNumber.js
@@ -1,10 +1,11 @@
 /**
  * Forward declaration of guess API.
  * @param {number} num   your guess
- * @return 	            -1 if num is lower than the guess number
- *			             1 if num is higher than the guess number
- *                       otherwise return 0
- * var guess = function(num) {}
+ * @return
+ *  -1 if num is lower than the guess number
+ *  1 if num is higher than the guess number
+ *  otherwise return 0
+ *  var guess = function(num) {}
  */
 
 /**
@@ -18,6 +19,28 @@ var guessNumber = function(n) {
 
   // if n = 1, return 1
   if (n === 1)  return 1;
+
+
+  // if guess returns -1, reutrn 1, returns 0
+
+  let mid = Math.floor(n / 2);
+
+  let low = 1;
+  let limit = n;
+
+  while (low <= limit) {
+    let mid = low + (high - low) / 2;
+    let result = guess(mid);
+
+    if (result === 0) {
+      return mid;
+    } else if (result < 0) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    return -1 ;
+  }
 
 };
 
