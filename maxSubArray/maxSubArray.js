edd203a035f128208d3ee32e158ce6813ded424c
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-28 12:36:12 -0700

    maxsubarray problem setup

---
+++ maxSubArray/maxSubArray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * 
+ * 
+ * A subarray is a contiguous part of an array.
+ */
+var maxSubArray = function(nums) {
+    
+};
+
+module.exports = maxSubArray;
---
+++ maxSubArray/maxSubArray.test.js
@@ -0,0 +1,9 @@
+const maxSub = require('./maxSubArray');
+
+describe('Max Sub Array', () => {
+
+  it('Should be a function', () => {
+    expect(typeof maxSub).toBe('function')
+  })
+
+});
