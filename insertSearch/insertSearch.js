fc790837e65c6a5b84935bc75489332941d3fb79
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-28 00:07:12 -0700

    passing local tests

--- insertSearch/insertSearch.js
+++ insertSearch/insertSearch.js
@@ -10,8 +10,39 @@
 var searchInsert = function(nums, target) {
   // return the index if target is found
 
+  // let index = 0; 
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] === target) {
+  //     index = i;
+  //   } else if (nums[i] > target && nums[i-1] < target) {
+  //     index = i;
+  //   } else {
+  //     index = nums.length;
+  //   }
+  // } 
+  // return index;
+
+  // iterate through the array
+  // if the target is found, return that index
+
+  // if the target is not found, find where it would be placed
+  // if target is < nums[0] return 0
+  // if target is > nums[length] return length
+
+  if (target < nums[0]) {
+    return 0;
+  }
+
+  if (target > nums[nums.length - 1]) {
+    return nums.length;
+  }
+
   for (let i = 0; i < nums.length; i++) {
-    
+    if (nums[i] === target) {
+      return i;
+    } else if (nums[i] > target && nums[i - 1] < target) {
+      return i;
+    }
   }
 };
 
--- insertSearch/insertSearch.test.js
+++ insertSearch/insertSearch.test.js
@@ -6,4 +6,13 @@ describe('Insert Search', () => {
     expect(typeof insertSearch).toBe('function');
   })
 
+  it('Should be correct', () => {
+    expect(insertSearch([1, 3, 5, 6], 5)).toBe(2);
+    expect(insertSearch([1, 3, 5, 6], 2)).toBe(1);
+    expect(insertSearch([1, 3, 5, 6], 7)).toBe(4);
+    expect(insertSearch([1, 3, 5, 6], 0)).toBe(0);
+    expect(insertSearch([1], 0)).toBe(0);
+  });
+
+
 });
