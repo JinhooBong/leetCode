9f34dba4141a17c40373b4e44c7b13b2199ace53
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-27 23:52:51 -0700

    search insert position problem set up

---
+++ insertSearch/insertSearch.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ * 
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+var searchInsert = function(nums, target) {
+  // return the index if target is found
+
+  for (let i = 0; i < nums.length; i++) {
+    
+  }
+};
+
+module.exports = searchInsert;
---
+++ insertSearch/insertSearch.test.js
@@ -0,0 +1,9 @@
+const insertSearch = require('./insertSearch');
+
+describe('Insert Search', () => {
+
+  it('Should be a function', () => {
+    expect(typeof insertSearch).toBe('function');
+  })
+
+});
