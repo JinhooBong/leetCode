4c39dc04a9318aba71751dd3c130429849ade78e
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-29 10:41:22 -0700

    length of last word problem set up

---
+++ lastWordLength/lastWordLength.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * 
+ * Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+ * 
+ * A word is a maximal substring consisting of non-space characters only.
+ * 
+ */
+var lengthOfLastWord = function(s) {
+    
+};
+
+module.exports = lengthOfLastWord;
---
+++ lastWordLength/lastWordLength.test.js
@@ -0,0 +1,9 @@
+const lastWord = require('./lastWordLength');
+
+describe('Length of Last Word', () => {
+
+  it('Should be a function', () => {
+    expect(typeof lastWord).toBe('function');
+  })
+
+})
