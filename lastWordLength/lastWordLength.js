f8833cec690910732ec07db2ab7252d7bd38b83f
Jinhoo Bong <jinhoobong@gmail.com>
2021-10-29 14:57:12 -0700

    length of last word passes local tests

--- lastWordLength/lastWordLength.js
+++ lastWordLength/lastWordLength.js
@@ -8,7 +8,34 @@
  * 
  */
 var lengthOfLastWord = function(s) {
-    
+    // split up the given string
+    // get last word in array length
+
+    // while iterating through string
+    // if it's a space, ignore
+    // else save letters as a word
+
+    // global word array variable
+    let words = [];
+    for (let i = 0; i < s.length; i++) {  
+      // if its space, skip
+      // if its not, then add it to a word and keep adding until space
+      let word = '';
+      if (s[i] !== ' ') {
+        // while current position is not space, keep adding
+        while (s[i] !== ' ' && i !== s.length - 1) {
+          word += s[i];
+          i++;
+        }
+        if (s[i] !== ' ') {
+          word += s[i];
+        }
+        // when while loop ends, it should be a word
+        words.push(word);
+      }
+    }
+
+    return words[words.length - 1].length;
 };
 
 module.exports = lengthOfLastWord;
--- lastWordLength/lastWordLength.test.js
+++ lastWordLength/lastWordLength.test.js
@@ -6,4 +6,10 @@ describe('Length of Last Word', () => {
     expect(typeof lastWord).toBe('function');
   })
 
+  it('Should be correct', () => {
+    expect(lastWord("   fly me   to   the moon  ")).toBe(4);
+    expect(lastWord('Hello World')).toBe(5);
+    expect(lastWord('luffy is still joyboy')).toBe(6);
+  })
+
 })
