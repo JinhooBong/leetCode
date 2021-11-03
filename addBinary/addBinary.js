12b30114acc8aac2996f133bdabc125665974059
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-02 23:32:35 -0700

    sqrt problem setup

--- addBinary/addBinary.js
+++ addBinary/addBinary.js
@@ -2,9 +2,14 @@
  * @param {string} a
  * @param {string} b
  * @return {string}
+ * 
+ * 
+ * Given two binary strings a and b, return their sum as a binary string.
+ * 
+ * 
  */
  var addBinary = function(a, b) {
-    
+    // 
 };
 
 module.exports = addBinary;
---
+++ sqrt/sqrt.test.js
@@ -0,0 +1,9 @@
+const sqrt = require('./sqrt');
+
+describe('Square Root', () => {
+  
+  it('Should be a function', () => {
+    expect(typeof sqrt).toBe('function');
+  })
+
+})
