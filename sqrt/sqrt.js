e18151c3c6b96817570075bf00998ea604da5287
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-03 17:34:46 -0700

    sqrt leetcode accepted

--- sqrt/sqrt.js
+++ sqrt/sqrt.js
@@ -10,8 +10,25 @@
  * 
  * 
  */
- var mySqrt = function(x) {
-    
+var mySqrt = function(x) {
+  let left = 1;
+  let right = x;
+
+  // the sqrt of 0 or 1 is itself
+  if (x < 2) return x;
+
+  // Use binary search to find the square root or the whole number closest to the square root
+  while (left < right) {
+    // find midpoint between left and right
+    const mid = Math.floor((right + left) / 2);
+    // if mid times itself is x, then it's the square root so return it
+    if (mid * mid === x) return mid;
+    // If mid squared is greater than x then the answer must be on the left half of mid
+    else if (mid * mid > x) right = mid;
+    // If mid squared is less than x then the answer must be on the right half of mid
+    else left = mid + 1;
+  }
+  return left - 1;
 };
 
 module.exports = mySqrt;
--- sqrt/sqrt.test.js
+++ sqrt/sqrt.test.js
@@ -6,4 +6,12 @@ describe('Square Root', () => {
     expect(typeof sqrt).toBe('function');
   })
 
+  it('Should be correct', () => {
+    expect(sqrt(4)).toBe(2)
+  })
+
+  it('Should round because truncate decimals', () => {
+    expect(sqrt(8)).toBe(2);
+  })
+
 })
