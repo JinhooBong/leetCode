a843fdc7c02e3102e3afb9406d3f3f4011899763
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-03 17:58:38 -0700

    addBinary problem leetcode solution accepted

--- addBinary/addBinary.js
+++ addBinary/addBinary.js
@@ -6,10 +6,37 @@
  * 
  * Given two binary strings a and b, return their sum as a binary string.
  * 
+ * Example: 
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * 
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
  * 
  */
  var addBinary = function(a, b) {
-    // 
+    // grab the length of each string 
+    // depending on the length of the string, we know which power of 2 we have 
+    // we then add it
+    // and then find to which power of 2 the result is
+    // and then we return the corresponding string
+
+    var len1 = a.length;    
+    var len2 = b.length;
+    var max = Math.max(len1, len2);
+    var res = '';
+    var carry = 0;
+    var val = 0;
+
+    for (var i = 0; i < max; i++) {
+        val = Number(a[len1 - 1 - i] || 0) + Number(b[len2 - 1 - i] || 0) + carry;
+        carry = Math.floor(val / 2);
+        res = (val % 2) + res;
+    }
+
+    if (carry) res = 1 + res;
+
+    return res;
 };
 
 module.exports = addBinary;
--- addBinary/addBinary.test.js
+++ addBinary/addBinary.test.js
@@ -6,4 +6,9 @@ describe('Add Binary', () => {
     expect(typeof addBinary).toBe('function');
   })
 
+  it('Example 1', () => {
+    expect(addBinary('11', '1')).toBe('100');
+    expect(addBinary('1010', '1011')).toBe('10101');
+  })
+
 })
