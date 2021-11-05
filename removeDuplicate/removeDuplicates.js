6c169c4dbdf687ee50fe77e30cf01feceef01ab8
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-05 16:38:22 -0700

    remove duplicate problem setup

---
+++ removeDuplicate/removeDuplicates.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * 
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+ * 
+ */
+var deleteDuplicates = function(head) {
+    
+};
+
+module.exports = deleteDuplicates;
