17908786b3a9470f3546ea26723e561050fa7c4f
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-09 17:20:36 -0800

    singlenum passing local tests

--- singleNum/singleNum.js
+++ singleNum/singleNum.js
@@ -9,6 +9,27 @@
  */
 var singleNumber = function(nums) {
     
+  // create an object
+  // iterate through array
+  // count how instances each elements appears
+  // return the one with 1
+
+  let countObj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (countObj[nums[i]] === undefined) {
+      countObj[nums[i]] = 1;
+    } else {
+      countObj[nums[i]]++;
+    }
+  }
+
+  for (let keys in countObj) {
+    if (countObj[keys] === 1) {
+      return parseInt(keys);
+    }
+  }
+
 };
 
 module.exports = singleNumber;
--- singleNum/singleNum.test.js
+++ singleNum/singleNum.test.js
@@ -6,4 +6,10 @@ describe('Single Number', () => {
     expect(typeof singleNum).toBe('function');
   });
 
+  it('Should be correct', () => {
+    expect(singleNum([2,2,1])).toBe(1);
+    expect(singleNum([4,1,2,1,2])).toBe(4);
+    expect(singleNum([1])).toBe(1);
+  })
+
 });
