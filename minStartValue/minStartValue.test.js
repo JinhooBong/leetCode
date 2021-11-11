efce466c3080890d297ad937cacd592cbcc91304
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-10 20:58:49 -0800

    setting up minstartvalue testing

---
+++ minStartValue/minStartValue.test.js
@@ -0,0 +1,9 @@
+const minStartValue = require('./minStartValue');
+
+describe('Minimum start value', () => {
+
+  it('Should be a function', () => {
+    expect(typeof minStartValue).toBe('function');
+  })
+
+})
