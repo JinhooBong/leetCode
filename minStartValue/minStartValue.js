c8a5f6bf58fe3a2da8e1af9606e8f6a14687f9c9
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-10 20:57:39 -0800

    setting up minstartvalue problem

---
+++ minStartValue/minStartValue.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * Given an array of integers nums, you start with an initial positive value startValue.
+ * 
+ * In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+ * 
+ * 
+ * Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+ * 
+ * 
+ */
+var minStartValue = function(nums) {
+    
+};
+
+module.exports = minStartValue;
