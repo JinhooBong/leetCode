de4047e87a0a34922af77bb1459c6f9b2a601aa7
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-16 20:40:42 -0800

    setting up uniquepath testing

---
+++ uniquePath/uniquePath.test.js
@@ -0,0 +1,9 @@
+const uniquePath = require('./uniquePath');
+
+describe('Unique Path Problem', () => {
+
+  it('Should be a function', () => {
+    expect(typeof uniquePath).toBe('function');
+  })
+
+})
