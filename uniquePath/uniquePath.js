283239edb6152d5d4236495dc990a77ed4c1dd2d
Jinhoo Bong <jinhoobong@gmail.com>
2021-11-16 20:39:44 -0800

    setting up uniquePath problem

---
+++ uniquePath/uniquePath.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ * 
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * 
+ * How many possible unique paths are there?
+ * 
+ */
+
+var uniquePaths = function(m, n) {
+    
+};
+
+module.exports = uniquePaths;
