93deded14489c978f2cc16f30ce5ef5b590377bd
Jinhoo Bong <jinhoobong@gmail.com>
2021-12-02 21:54:24 -0800

    max product subarray passes local tests

--- maxProductSubArray/maxProdSubArray.js
+++ maxProductSubArray/maxProdSubArray.js
@@ -1,9 +1,38 @@
 /**
+ * Given an integer array nums, find a contiguous non-empty
+ * subarray within the array that has the largest product, and
+ * return the product.
+ * 
+ * It is guaranteed that the answer will fit in a 32-bit integer.
+ * 
+ * A subarray is a contiguous subsequence of the array.
+ * 
  * @param {number[]} nums
  * @return {number}
  */
- var maxProduct = function(nums) {
-    
+var maxProduct = function(nums) {
+  // if our values are all positive, it's always going to be increasing thus, the max would be a product of all values
+
+  // if all values are negative, sign wil alternate
+
+
+  // init max
+  let res = Math.max(...nums);
+  let currMin = 1;
+  let currMax = 1;
+
+  for (const n of nums) {
+    if (n === 0) {
+       currMin = 1;
+       currMax = 1;
+    } 
+    let temp = currMax * n;
+    currMax = Math.max(n * currMax, n * currMin, n);
+    currMin = Math.min(temp, n * currMin, n);
+    res = Math.max(res, currMax);    
+  }
+  
+  return res;
 };
 
 module.exports = maxProduct;
--- maxProductSubArray/maxProdSubArray.test.js
+++ maxProductSubArray/maxProdSubArray.test.js
@@ -6,4 +6,12 @@ describe('Maximum Product of Subarray', () => {
     expect(typeof maxProd).toBe('function');
   })
 
+  it('Example 1', () => {
+    expect(maxProd([2, 3, -2, 4])).toBe(6);
+  })
+
+  it('Example 2', () => {
+    expect(maxProd([-2, 0, -1])).toBe(0);
+  })
+
 });
