81f7e1c855cb09a45c12de592500a1afa3228691
Jinhoo Bong <jinhoobong@gmail.com>
2021-12-06 19:12:49 -0800

    set up testing for getDecimalValue problem

---
+++ getDecimalValue/getDecimalValue.test.js
@@ -0,0 +1,9 @@
+const getDecimalValue = require('./getDecimalValue');
+
+describe('Get Decimal Value', () => {
+
+  it('Should be a function', () => {
+    expect(typeof getDecimalValue).toBe('function');
+  })
+
+});
