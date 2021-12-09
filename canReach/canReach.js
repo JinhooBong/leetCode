69fcc01804a186dea7b3cf4f04349cfd16a0fdab
Jinhoo Bong <jinhoobong@gmail.com>
2021-12-08 22:49:25 -0800

    canReach passes local tests;

--- canReach/canReach.js
+++ canReach/canReach.js
@@ -13,7 +13,21 @@
  * 
  */
 var canReach = function(arr, start) {
-    
+    // return a boolean value whether we can reach an index in the array where the value is 0 given the specific conditions
+
+    // if the array doesn't contain a 0, then return false
+    if (!arr.includes(0)) return false;
+
+    let dfs = (arr, start, count) => {
+      if (start < 0 || start > arr.length) return false;
+      if (count >= arr.length) return false;
+      if (arr[start] === 0) return true;
+
+      return dfs(arr, start + arr[start], count + 1) ||
+        dfs(arr, start - arr[start], count + 1)
+    }
+
+    return dfs(arr, start, 0);
 };
 
 module.exports = canReach;
--- canReach/canReach.test.js
+++ canReach/canReach.test.js
@@ -6,4 +6,16 @@ describe('Can Reach', () => {
     expect(typeof canReach).toBe('function');
   })
 
+  it('Example 1', () => {
+    expect(canReach([4,2,3,0,3,1,2], 5)).toBe(true);
+  })
+
+  it('Example 2', () => {
+    expect(canReach([4,2,3,0,3,1,2], 0)).toBe(true);
+  })
+
+  it('Example 3', () => {
+    expect(canReach([3,0,2,1,2], 2)).toBe(false)
+  })
+
 })
