45693a37dafed035fe8efd426a53091ba320b406
Jinhoo Bong <jinhoobong@gmail.com>
2021-12-15 11:18:26 -0800

    insertion sort list problem set up

---
+++ insertionSortList/insertionSortList.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * 
+ * 
+ * Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+ * 
+ * The steps of the insertion sort algorithm:
+ * 
+ * Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+ * At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and
+ * inserts it there.
+ * 
+ * It repeats until no input elements remain.
+ * The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first
+ * element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.
+ * 
+ */
+var insertionSortList = function(head) {
+    
+};
+
+module.exports = insertionSortList;
