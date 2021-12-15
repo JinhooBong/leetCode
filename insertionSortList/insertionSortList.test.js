cf0ff7d3b729ae037e869cc20814c6e9fc186b20
Jinhoo Bong <jinhoobong@gmail.com>
2021-12-15 11:19:32 -0800

    insertion sort list test set up;

---
+++ insertionSortList/insertionSortList.test.js
@@ -0,0 +1,9 @@
+const insertionSort = require('./insertionSortList');
+
+describe('Insetion Sort List Problem', () => {
+
+  it('Should be a function', () => {
+    expect(typeof insertionSort).toBe('function');
+  })
+
+})
