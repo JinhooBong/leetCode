5b68690ede86e021e7343b7dac5a0ae9a40ff4e1
Jinhoo Bong <jinhoobong@gmail.com>
2022-02-07 23:00:22 -0800

    set up addDigits problem

---
+++ addDigits/addDigits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ * 
+ * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+ * 
+ */
+var addDigits = function(num) {
+  // create sum variable
+  // take each digit from num variable
+  // and then add it to sum variable
+
+
+};
+
+module.exports = addDigits;
