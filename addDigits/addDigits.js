6ae78c15ac15e23909eeb0f467d4d4cc7d03cc77
Jinhoo Bong <jinhoobong@gmail.com>
2022-02-07 23:24:57 -0800

    passes local tests

--- addDigits/addDigits.js
+++ addDigits/addDigits.js
@@ -10,7 +10,32 @@ var addDigits = function(num) {
   // take each digit from num variable
   // and then add it to sum variable
 
+  let sum = 0;
 
+  // if number is one digit, sum will be the number
+  if (num < 10) {
+    return num;
+  } else {
+    sum = helper(num);
+    while (sum > 10) {
+      sum = helper(sum);
+    }
+  }
+
+  return sum;
 };
+
+let helper = (num) => {
+  let sum = 0;
+
+  while (num >= 10) {
+    let digit = num % 10;
+    sum += digit;
+    num = Math.floor(num / 10);
+  }
+
+  sum += num; 
+  return sum;
+}
 
 module.exports = addDigits;
--- addDigits/addDigits.test.js
+++ addDigits/addDigits.test.js
@@ -6,4 +6,19 @@ describe('Add Digits', () => {
     expect(typeof addDigits).toBe('function');
   })
 
+  it('Should handle single digits', () => {
+    expect(addDigits(2)).toBe(2);
+  })
+
+  it('Should handle double digits', () => {
+    expect(addDigits(0)).toBe(0);
+  })
+
+  it('Should handle double digits', () => {
+    expect(addDigits(11)).toBe(2);
+    expect(addDigits(38)).toBe(2);
+    expect(addDigits(10)).toBe(1);
+    expect(addDigits(19)).toBe(1);
+  })
+
 })
